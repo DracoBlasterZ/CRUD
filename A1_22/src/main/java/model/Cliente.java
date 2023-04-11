d1f9523e33a000d7ae0e207dae44a1c474a9fb77
camilagarcia36 <garciiaperezz@gmail.com>
2023-04-11 13:05:50 +0200

    controler creado

--- A1_22/src/main/java/model/Cliente.java
+++ A1_22/src/main/java/model/Cliente.java
@@ -16,18 +16,17 @@ public class Cliente {
 		dbc.selectDB("clientes");
 
 		// Crear tabla cliente
-		String tableName1 = "cliente";
 		String tableColumns1 = "id INT AUTO_INCREMENT, nombre VARCHAR(100),"
-				+ " apellido VARCHAR(100),direccion VARCHAR(300),dni char(8), fecha datatime, PRIMARY KEY (id)";
-		dbc.createTable("cliente", tableName1, tableColumns1);
+				+ " apellido VARCHAR(100),direccion VARCHAR(300),dni char(8), fecha date, PRIMARY KEY (id)";
+		dbc.createTable("clientes", "cliente", tableColumns1);
 
 		// Insertar registros en la tabla cliente
-		String columns1 = "id, nombre, apellido, direccion, dni, fecha";
-		String values1_1 = "1, 'Pepito','Bonito','Bajo un puente', 64587573, 2000-11-08";
-		dbc.insertData("ud18_ejercicio2", tableName1, columns1, values1_1);
+		String columns1 = "nombre, apellido, direccion, dni, fecha";
+		String values1_1 = "'Juan', 'Pérez', 'Calle Falsa 123', '12345678', '2023-04-11'";
+		dbc.insertData("clientes", "cliente", columns1, values1_1);
 
 
-		// Enseñar y Eliminar datos en la tabla
+		// Enseñar datos
 		dbc.getValues("clientes", "cliente", columns1);
 
 		// Cerrar conexión
