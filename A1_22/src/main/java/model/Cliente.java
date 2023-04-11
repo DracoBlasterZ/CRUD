db4996299c660e368bafbbe02aafbd39ae0db80d
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-04-11 12:12:07 +0200

    Update

--- A1_22/src/main/java/model/Cliente.java
+++ A1_22/src/main/java/model/Cliente.java
@@ -10,40 +10,25 @@ public class Cliente {
 		dbc.connect("192.168.4.105:3306", "remote", "P@ssw0rd_Remote");
 
 		// Elimina y crea la base de datos ud18_ejercicio2
-		dbc.createDB("ud18_ejercicio2");
+		dbc.createDB("clientes");
 
 		// Selecciona la base de datos
-		dbc.selectDB("ud18_ejercicio2");
+		dbc.selectDB("clientes");
 
-		// Crear tabla departamentos
-		String tableName1 = "departamentos";
-		String tableColumns1 = "cod_departamento INT AUTO_INCREMENT, nombre VARCHAR(100), presupuesto INT, PRIMARY KEY (cod_departamento)";
-		dbc.createTable("ud18_ejercicio2", tableName1, tableColumns1);
+		// Crear tabla cliente
+		String tableName1 = "cliente";
+		String tableColumns1 = "id INT AUTO_INCREMENT, nombre VARCHAR(100),"
+				+ " apellido VARCHAR(100),direccion VARCHAR(300),dni char(8), fecha datatime, PRIMARY KEY (id)";
+		dbc.createTable("cliente", tableName1, tableColumns1);
 
-		// Crear tabla empleados
-		String tableName2 = "empleados";
-		String tableColumns2 = "dni VARCHAR(8), nombre VARCHAR(100), apellidos VARCHAR(255), departamento INT, PRIMARY KEY (dni), FOREIGN KEY (departamento) REFERENCES departamentos(cod_departamento) ON DELETE CASCADE ON UPDATE CASCADE";
-		dbc.createTable("ud18_ejercicio2", tableName2, tableColumns2);
-
-		// Insertar registros en la tabla departamentos
-		String columns1 = "cod_departamento, nombre, presupuesto";
-		String values1_1 = "1, 'Recursos Humanos', 50000";
-		String values1_2 = "2, 'Desarrollo', 80000";
-		String values1_3 = "3, 'Desarrollo', 80000";
+		// Insertar registros en la tabla cliente
+		String columns1 = "id, nombre, apellido, direccion, dni, fecha";
+		String values1_1 = "1, 'Pepito','Bonito','Bajo un puente', 64587573, 2000-11-08";
 		dbc.insertData("ud18_ejercicio2", tableName1, columns1, values1_1);
-		dbc.insertData("ud18_ejercicio2", tableName1, columns1, values1_2);
-		dbc.insertData("ud18_ejercicio2", tableName1, columns1, values1_3);
 
-		// Insertar registros en la tabla empleados
-		String columns2 = "dni, nombre, apellidos, departamento";
-		String values2_1 = "'12345678', 'Juan', 'Gonzalez', 1";
-		String values2_2 = "'23456789', 'Esther', 'Cruz', 2";
-		dbc.insertData("ud18_ejercicio2", tableName2, columns2, values2_1);
-		dbc.insertData("ud18_ejercicio2", tableName2, columns2, values2_2);
 
 		// Enseñar y Eliminar datos en la tabla
-		dbc.getValues("ud18_ejercicio2", "departamentos", columns1);
-		dbc.deleteRecord("ud18_ejercicio2.departamentos", "cod_departamento", "1");
+		dbc.getValues("clientes", "cliente", columns1);
 
 		// Cerrar conexión
 		dbc.closeConnection();
