826b01798abb86a5f8ce6d08c6cc14c408844ea8
camilagarcia36 <garciiaperezz@gmail.com>
2023-04-17 00:02:57 +0200

    Revert "Delete A1_22 directory"
    This reverts commit 32a82c98c2661283f1bc2783f0b0eb65c27317a9.

---
+++ A1_22/src/main/java/model/Cliente.java
@@ -0,0 +1,135 @@
+package model;
+
+
+
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Cliente {
+
+	// Client model with attributes
+	private int id;
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private int DNI;
+	private String date;
+
+	// Default constructor
+	public Cliente() {
+	}
+
+	// Constructor with attributes
+	public Cliente(int id, String nombre, String apellido, String direccion, int DNI, String date) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.direccion = direccion;
+		this.DNI = DNI;
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", direccion=" + direccion
+				+ ", DNI=" + DNI + ", date=" + date + "";
+	}
+
+	// Getters
+	public int getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public int getDNI() {
+		return DNI;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	// Setters
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = strLenValidator(nombre, 250);
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = strLenValidator(apellido, 250);
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = strLenValidator(direccion, 250);
+	}
+
+	public void setDNI(int DNI) {
+		this.DNI = DNIValidator(DNI);
+	}
+
+	public void setDate(String date) {
+		this.date = dateValidator(date);
+	}
+
+	// Methods
+	// Validators
+	// Validate x character varchars
+	public String strLenValidator(String string, int l) {
+		if (string.length() > l) {
+			JOptionPane.showMessageDialog(null, string + ". This field is longer than " + l + " characters.");
+			return ""; // If invalid set empty string
+		}
+		return string;
+	}
+
+	// Validate for DNI length between 8 and 11
+	public int DNIValidator(int DNI) {
+		int length = String.valueOf(DNI).length();
+		if (length < 8 || length > 11) {
+			JOptionPane.showMessageDialog(null, "Dni invalid, setting default 0");
+			return 0; // If invalid set 0
+		}
+		return DNI;
+	}
+
+	// Validate date format is valid and returns as String
+	public String dateValidator(String date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setLenient(false);
+
+		try {
+			Date SQLdate = (Date) sdf.parse(date);
+			String formattedDate = sdf.format(SQLdate);
+			if (formattedDate.equals(date)) {
+				return formattedDate;
+			}
+		} catch (ParseException e) {  // If invalid set null
+			JOptionPane.showMessageDialog(null, "Fecha invalid format yyyy-mm-dd, setting NULL");
+			return null;
+		} catch (NullPointerException e) {
+			JOptionPane.showMessageDialog(null, "Fecha invalid format yyyy-mm-dd, setting NULL");
+			return null;
+		}
+		JOptionPane.showMessageDialog(null, "Fecha invalid format yyyy-mm-dd, setting NULL");
+		return null;
+	}
+
+}
---
+++ A1_22/src/main/java/model/DbConection.java
@@ -0,0 +1,138 @@
+package model;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+
+public class DbConection {
+	private Connection conexion;
+
+	public void connect(String IP, String user, String password) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection("jdbc:mysql://" + IP + " ", user, password);
+			System.out.print("Server Connected");
+		} catch (SQLException | ClassNotFoundException ex) {
+			System.out.print("No se ha podido conectar con mi base de datos");
+			System.out.print(ex);
+		}
+
+	}
+
+	public void closeConnection() {
+		try {
+			conexion.close();
+			JOptionPane.showMessageDialog(null, "Se ha finalizado la conexion con el servidor");
+		} catch (SQLException ex) {
+			Logger.getLogger(DbConection.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	public void createDB(String dbName) {
+
+		try {
+			String kuery = " CREATE DATABASE IF NOT EXISTS " + dbName + " ;";
+			Statement st = conexion.createStatement();
+			System.out.println(kuery);
+			st.executeUpdate(kuery);
+			System.out.println("Se ha creado la base de datos" + dbName + " correctamente");
+		} catch (SQLException ex) {
+			Logger.getLogger(DbConection.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	// Metodo que se asegura que se esta usando la BBDD
+	public void selectDB(String dbName) {
+		try {
+			String queryDb = "USE " + dbName + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(queryDb);
+		} catch (SQLException ex) {
+			Logger.getLogger(DbConection.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	// Método para crear una tabla en una base de datos
+	public void createTable(String nombreBBDD, String nombreTabla, String tablaColum) {
+		try {
+			String queryDb = "USE " + nombreBBDD + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(queryDb);
+
+			String query = "CREATE TABLE IF NOT EXISTS " + nombreTabla + " (" + tablaColum + ");";
+			Statement st = conexion.createStatement();
+			st.executeUpdate(query);
+
+			System.out.println("Se ha creado la tabla " + nombreTabla + " correctamente");
+		} catch (SQLException ex) {
+			Logger.getLogger(DbConection.class.getName()).log(Level.SEVERE, null, ex);
+			JOptionPane.showMessageDialog(null, "Error creando la tabla " + nombreTabla);
+		}
+	}
+
+	// Método para insertar datos en una tabla
+	public void insertData(String dbName, String tableName, String columns, String values) {
+		try {
+			String queryDb = "USE " + dbName + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(queryDb);
+
+			String query = "INSERT INTO " + tableName + " (" + columns + ") VALUES (" + values + ");";
+			Statement st = conexion.createStatement();
+			st.executeUpdate(query);
+
+			System.out.println("Datos insertados correctamente en la tabla " + tableName);
+		} catch (SQLException ex) {
+			Logger.getLogger(DbConection.class.getName()).log(Level.SEVERE, null, ex);
+			JOptionPane.showMessageDialog(null, "Error insertando datos en la tabla " + tableName);
+		}
+	}
+
+	// METODO QUE OBTIENE VALORES MYSQL
+	public void getValues(String db, String table_name, String columnas) {
+		String[] columna = columnas.split(",");
+
+		try {
+			String queryDb = "USE " + db + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(queryDb);
+
+			String query = "SELECT * FROM " + table_name;
+			Statement st = conexion.createStatement();
+			java.sql.ResultSet resultSet;
+			resultSet = st.executeQuery(query);
+
+			System.out.println("Has creado la tabla " + table_name + " con los siguientes valores:");
+			while (resultSet.next()) {
+
+				for (String valor : columna) {
+					System.out.println(resultSet.getString(valor.trim()));
+				}
+
+			}
+
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println("Error en la adquisicion de datos");
+		}
+	}
+
+	// METODO QUE ELIMINA VALORES DE NUESTRA BASE DE DATOS
+	public void deleteRecord(String table_name_columna, String columna, String campo) {
+		try {
+			String query = "DELETE FROM " + table_name_columna + " WHERE " + columna + "= \"" + campo + "\"";
+			Statement st = conexion.createStatement();
+			st.executeUpdate(query);
+
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+			JOptionPane.showMessageDialog(null, "Error borrando el registro especificado");
+		}
+	}
+
+}
